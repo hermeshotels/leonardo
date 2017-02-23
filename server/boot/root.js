4ba191789a93d8326475310bcf0cbd8f93552830
Wanny Miarelli <wannymiarelli@live.com>
2017-02-23 15:05:57 +0100

    socketio route handling

--- server/boot/root.js
+++ server/boot/root.js
@@ -4,5 +4,8 @@ module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
   router.get('/', server.loopback.status());
+  router.get('/socket.io', (req, res) => {
+    res.send('');
+  });
   server.use(router);
 };
