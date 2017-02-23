a265136eab6cb65fd245ac53ee6ac7b12708d460
Wanny Miarelli <wannymiarelli@live.com>
2017-02-23 15:10:10 +0100

    changes socket io post and get

--- server/boot/root.js
+++ server/boot/root.js
@@ -4,6 +4,9 @@ module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
   router.get('/', server.loopback.status());
+  router.get('/socket.io', (req, res) => {
+    res.send('');
+  });
   router.post('/socket.io', (req, res) => {
     res.send('');
   });
