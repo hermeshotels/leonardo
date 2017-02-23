5791668b856c57045c4cbee5094156ddcd88e34e
Wanny Miarelli <wannymiarelli@live.com>
2017-02-23 15:08:24 +0100

    socketio changed to pos

--- server/boot/root.js
+++ server/boot/root.js
@@ -4,7 +4,7 @@ module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
   router.get('/', server.loopback.status());
-  router.get('/socket.io', (req, res) => {
+  router.post('/socket.io', (req, res) => {
     res.send('');
   });
   server.use(router);
