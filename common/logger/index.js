3e54808123e69399f5bad8d7c8479b0c9ce08e7e
Wanny Miarelli <wannymiarelli@live.com>
2017-05-12 17:09:03 +0200

    Json logging enabled

--- common/logger/index.js
+++ common/logger/index.js
@@ -2,11 +2,15 @@ import winston from 'winston'
 let logger = new (winston.Logger)({
   exitOnError: false,
   transports: [
-    new (winston.transports.Console)({ level: 'verbose' }),
+    new (winston.transports.Console)({
+      level: 'verbose',
+      colorize: true
+    }),
     new (winston.transports.File)({
       filename: 'godblessyou.log',
       level: 'verbose',
-      timestamp: true
+      timestamp: true,
+      json: true
     })
   ]
 })
